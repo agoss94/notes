1c6bde22327e07be99a53354d276c616f74c95d1
Andreas Goss <andreas.goss1994@gmail.com>
2025-01-17 15:43:55 +0100

    Anpassung request handlers

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,17 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
 
+const errorHandler = (error, request, response, next) => { 
+    console.error(error.message)
+
+    if (error.name === 'CastError') { 
+        return response.status(400).send({error: 'malformatted id'})
+    }
+
+    next(error)
+}
+app.use(errorHandler)
+
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
@@ -17,17 +28,23 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     const id = request.params.id
-    Note.findById(id).then(result => response.json(result))
-
+    Note.findById(id).then(note => {
+        if(note) { 
+            response.json(note)
+        } else { 
+            response.status(404).end()
+        }
+    }).catch(error => next(error))
 
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => {
-        response.json(note)
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id).then(result => {
+        response.status(204).end()
     })
+    .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
@@ -48,6 +65,22 @@ app.post('/api/notes', (request, response) => {
         response.json(savedNote)
     })
 })
+
+app.put('/api/notes/:id', (request, response, next) => { 
+    const body = request.body
+
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+
+    Note.findByIdAndUpdate(request.params.id,  note, { new: true}).then(
+        updatedNote => { 
+            response.json(updatedNote)
+        }
+    ).catch(error => next(error))
+})
+
 const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
