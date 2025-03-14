f3947803e25d5c42284da87b60aec487ac6a5722
Andreas Goss <andreas.goss1994@gmail.com>
2025-03-14 11:26:53 +0100

    Add untracked files

---
+++ backend/mongo.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://agoss:${password}@fullstackdb.7wk3e.mongodb.net/testNoteApp?retryWrites=true&w=majority&appName=FullstackDB`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url).then(() => {
+    const noteSchema = new mongoose.Schema({
+        content: String,
+        important: Boolean,
+    })
+
+    const Note = mongoose.model('Note', noteSchema)
+
+    const note = new Note({
+        content: 'CSS is awesome',
+        important: true,
+    })
+
+    note.save().then(() => {
+        console.log('note saved!')
+        mongoose.connection.close()
+    })
+
+    Note.find({}).then(result => {
+        result.forEach(note => {
+            console.log(note)
+        })
+        mongoose.connection.close()
+    })
+})
